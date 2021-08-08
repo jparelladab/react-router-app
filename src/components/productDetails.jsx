0279cfc82141d7e8ea8c3e246d61cbe56f0bf915
joan parellada <joan.parellada789@gmail.com>
2021-08-08 19:37:34 +0200

    added history push to productDetails save button

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -3,6 +3,9 @@ import React, { Component } from "react";
 class ProductDetails extends Component {
   handleSave = () => {
     // Navigate to /products
+    // with push you navigate to the path and can go back, with replace you can't go back
+    //replace is often used in login pages
+    this.props.history.push('/products');
   };
 
   render() {
